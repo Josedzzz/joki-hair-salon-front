5b815c47eeefab911e022188db79b52e633cefa4
joki <josedavidamayar@gmail.com>
2024-10-14 19:30:12 -0500

    add some custom colors for the app

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-dark": "#0A0A0A",
+        "custom-platinum": "#E0E0E0",
+        "custom-white": "#F0F0F0",
+      },
+    },
   },
   plugins: [],
 };
