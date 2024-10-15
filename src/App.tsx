59c906d3bf730da2141e93d9e8db86a1da91659d
joki <josedavidamayar@gmail.com>
2024-10-14 19:31:45 -0500

    use privane routes for the user and admin dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,58 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import LoginDashboard from "./components/LoginDashboard";
+import AdminDashboard from "./components/AdminDashboard";
+import UserDashboard from "./components/UserDashboard";
+
+/**
+ * private route for the admin to navigate to it's dashboard
+ * @param param0 the jsx element
+ * @returns
+ */
+function PrivateRouteAdmin({ children }: { children: JSX.Element }) {
+  const adminId = localStorage.getItem("adminId");
+  return adminId ? children : <Navigate to={"/login"} />;
+}
+
+/**
+ * private route for the user to navigate to it's dashboard
+ * @param param0 the jsx element
+ * @returns
+ */
+function PrivateRouteUser({ children }: { children: JSX.Element }) {
+  const userId = localStorage.getItem("userId");
+  return userId ? children : <Navigate to={"/login"} />;
+}
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-400">Hello world!</h1>
+    <Router>
+      <div className="min-h-screen bg-custom-platinum">
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/login" element={<LoginDashboard />} />
+          <Route
+            path="/admin-dashboard"
+            element={
+              <PrivateRouteAdmin>
+                <AdminDashboard />
+              </PrivateRouteAdmin>
+            }
+          />
+          <Route
+            path="/user-dashboard"
+            element={
+              <PrivateRouteUser>
+                <UserDashboard />
+              </PrivateRouteUser>
+            }
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
