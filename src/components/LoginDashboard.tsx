e4532c269a80e5e93214822f6684c41679abd60f
joki <josedavidamayar@gmail.com>
2024-10-16 03:06:12 -0500

    add conditional logic to render content based on the card selected

--- src/components/LoginDashboard.tsx
+++ src/components/LoginDashboard.tsx
@@ -1,3 +1,27 @@
+import { useState } from "react";
+import LoginHeader from "./LoginHeader";
+import LoginCard from "./LoginCard";
+import SignupCard from "./SignupCard";
+import AboutCard from "./AboutCard";
+
 export default function LoginDashboard() {
-  return <h1>Login test</h1>;
+  // use state to set the card to display on the login dashboard
+  const [card, setCard] = useState<string>("login");
+
+  /**
+   * change the window content based on the card name
+   * @param cardName the name of the car to display
+   */
+  const toggleCard = (cardName: string) => {
+    setCard(cardName);
+  };
+
+  return (
+    <div className="min-h-screen bg-custom-black flex flex-col">
+      <LoginHeader toggleCard={toggleCard} card={card} />
+      {card === "login" && <LoginCard />}
+      {card === "singup" && <SignupCard />}
+      {card === "aboutus" && <AboutCard />}
+    </div>
+  );
 }
