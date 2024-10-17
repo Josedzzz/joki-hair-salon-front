7739f71bce324a918d6a74944447e1d5870393f4
joki <josedavidamayar@gmail.com>
2024-10-17 16:53:35 -0500

    add a custom background to the app

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,14 @@ function PrivateRouteUser({ children }: { children: JSX.Element }) {
 export default function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-custom-platinum">
+      <div
+        className="min-h-screen"
+        style={{
+          background:
+            "linear-gradient(135deg,#0000 20.5%,#e0e0e0 0 29.5%,#0000 0) 0 31px, linear-gradient(45deg,#0000 8%,#e0e0e0 0 17%, #0000 0 58%) 62px 0, linear-gradient(135deg,#0000 8%,#e0e0e0 0 17%, #0000 0 58%,#e0e0e0 0 67%,#0000 0), linear-gradient(45deg,#0000 8%,#e0e0e0 0 17%, #0000 0 58%,#e0e0e0 0 67%,#0000 0 83%,#e0e0e0 0 92%,#0000 0), #f0f0f0",
+          backgroundSize: "124px 124px",
+        }}
+      >
         <Routes>
           <Route path="/" element={<Navigate to="/login" />} />
           <Route path="/login" element={<LoginDashboard />} />
