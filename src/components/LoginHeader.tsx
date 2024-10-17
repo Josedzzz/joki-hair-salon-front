b2007124699693c4d5324e0839a1eb7563c79f97
joki <josedavidamayar@gmail.com>
2024-10-17 16:55:04 -0500

    add a white background to the header

--- src/components/LoginHeader.tsx
+++ src/components/LoginHeader.tsx
@@ -22,7 +22,7 @@ export default function LoginHeader({ toggleCard, card }: LoginHeaderProps) {
     }`;
 
   return (
-    <header className="mt-3 h-12 w-full flex justify-between items-center max-w-3xl mx-auto p-2">
+    <header className="h-16 w-full bg-custom-white flex justify-between items-center p-2 sm:px-14">
       <div className="flex items-center space-x-1 gap-3">
         <h1 className="text-xl sm:text-3xl md:text-4xl font-bold text-custom-dark">
           Joki Hair Salon
@@ -40,7 +40,7 @@ export default function LoginHeader({ toggleCard, card }: LoginHeaderProps) {
       <nav
         className={`${
           menuOpen ? "block" : "hidden"
-        } sm:flex space-x-4 sm:space-x-8 absolute sm:relative top-16 sm:top-auto left-0 w-full sm:w-auto bg-custom-platinum sm:bg-transparent p-4 sm:p-0`}
+        } sm:flex space-x-4 sm:space-x-8 absolute sm:relative top-16 sm:top-auto left-0 w-full sm:w-auto bg-custom-white sm:bg-transparent p-4 sm:p-0`}
       >
         <a
           onClick={() => toggleCard("aboutus")}
