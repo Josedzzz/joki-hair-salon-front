dfeac2c30a4a5b3f46dcc861992f40dbfb38d4d6
joki <josedavidamayar@gmail.com>
2024-10-20 03:42:29 -0500

    call the components to render

--- src/components/UserDashboard.tsx
+++ src/components/UserDashboard.tsx
@@ -1,3 +1,25 @@
+import { useState } from "react";
+import UserHeader from "./UserHeader";
+import UserHome from "./UserHome";
+import UserAppointment from "./UserAppointment";
+
 export default function UserDashboard() {
-  return <h1>User dashboard test</h1>;
+  // state to set the content
+  const [content, setContent] = useState("home");
+
+  /**
+   * change the window content
+   * @param contentName the name of the content to display
+   */
+  const toggleContent = (contentName: string) => {
+    setContent(contentName);
+  };
+
+  return (
+    <div className="min-h-screen flex flex-col">
+      <UserHeader toggleContent={toggleContent} content={content} />
+      {content === "home" && <UserHome />}
+      {content === "appointment" && <UserAppointment />}
+    </div>
+  );
 }
