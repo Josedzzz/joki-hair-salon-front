630290b30cce3ab5ec2a9836d81575f5bf33ba85
Josedzzz <josedavidamayar@gmail.com>
2024-10-25 14:19:00 -0500

    change the bacground

--- src/components/UserHome.tsx
+++ src/components/UserHome.tsx
@@ -1,3 +1,14 @@
 export default function UserHome() {
-  return <h1>UserHome component test</h1>;
+  return (
+    <div
+      className="w-full min-h-[calc(100vh-4rem)] p-6"
+      style={{
+        background:
+          "linear-gradient(135deg,#0000 20.5%,#e0e0e0 0 29.5%,#0000 0) 0 31px, linear-gradient(45deg,#0000 8%,#e0e0e0 0 17%, #0000 0 58%) 62px 0, linear-gradient(135deg,#0000 8%,#e0e0e0 0 17%, #0000 0 58%,#e0e0e0 0 67%,#0000 0), linear-gradient(45deg,#0000 8%,#e0e0e0 0 17%, #0000 0 58%,#e0e0e0 0 67%,#0000 0 83%,#e0e0e0 0 92%,#0000 0), #f0f0f0",
+        backgroundSize: "124px 124px",
+      }}
+    >
+      <div></div>
+    </div>
+  );
 }
