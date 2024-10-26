5de457369a004097d02170144eaa0bcde0caab81
Josedzzz <josedavidamayar@gmail.com>
2024-10-26 01:14:02 -0500

    change the dashborad content using the header links

--- src/components/AdminDashboard.tsx
+++ src/components/AdminDashboard.tsx
@@ -1,3 +1,27 @@
+import { useState } from "react";
+import AdminHeader from "./AdminHeader";
+import AdminAppointment from "./AdminAppointment";
+import AdminEmployee from "./AdminEmployee";
+import AdminProduct from "./AdminProduct";
+
 export default function AdminDashboard() {
-  return <h1 className="text-red-400 font-bold">AdminDashboard hola</h1>;
+  // state to set the content
+  const [content, setContent] = useState("employee");
+
+  /**
+   * change the window content
+   * @param contentName the name of the content to display
+   */
+  const toggleContent = (contentName: string) => {
+    setContent(contentName);
+  };
+
+  return (
+    <div className="min-h-screen flex flex-col">
+      <AdminHeader toggleContent={toggleContent} content={content} />
+      {content === "employee" && <AdminEmployee />}
+      {content === "product" && <AdminProduct />}
+      {content === "appointment" && <AdminAppointment />}
+    </div>
+  );
 }
