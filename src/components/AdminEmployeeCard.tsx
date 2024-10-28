b516f1ac7663284e2fad897d1eb15dd4225a9837
Josedzzz <josedavidamayar@gmail.com>
2024-10-27 23:06:11 -0500

    add a hover efect to the card

--- src/components/AdminEmployeeCard.tsx
+++ src/components/AdminEmployeeCard.tsx
@@ -31,7 +31,7 @@ export default function AdminEmployeeCard({
   return (
     <div
       onClick={onClick}
-      className="bg-custom-platinum rounded-2xl shadow-lg overflow-hidden max-w-xs border-4 border-transparent hover:border-custom-dark transition duration-300 ease-in-out"
+      className="bg-custom-platinum rounded-2xl shadow-lg overflow-hidden max-w-xs border-4 border-transparent hover:border-custom-dark transition duration-300 ease-in-out transform hover:scale-105"
     >
       <div className="p-4">
         <h3 className="text-lg font-bold text-custom-dark mb-2">
