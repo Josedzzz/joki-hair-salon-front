085804cf1e20a0a5aa748ef6ce4e5d907d506cd7
Josedzzz <josedavidamayar@gmail.com>
2024-11-05 16:07:36 -0500

    change the component style

--- src/components/UserCartCard.tsx
+++ src/components/UserCartCard.tsx
@@ -11,22 +11,22 @@ export default function UserCartCard({
 }: UserCartCardProps) {
   return (
     <div className="bg-custom-platinum rounded-2xl shadow-lg overflow-hidden max-w-xs border-4 border-transparent hover:border-custom-dark transition duration-300 ease-in-out transform hover:scale-105">
-      <div className="relative p-4 text-white flex flex-col justify-between h-full">
+      <div className="relative p-4 text-custom-dark flex flex-col justify-between h-full">
         <div>
           <h3 className="text-lg font-bold">{productCart.productName}</h3>
-          <p className="text-lg font-semibold">${productCart.brandName}</p>
+          <p className="text-lg font-semibold">{productCart.brandName}</p>
         </div>
 
         <div>
-          <p className="text-lg font-semibold">${productCart.totalPrice}</p>
-          <p className="text-xs opacity-70">
-            Quantity: {productCart.selectQty}
+          <p className="text-lg font-normal">${productCart.totalPrice}</p>
+          <p className="text-lg font-normal">
+            Quantity: {productCart.selectedQty}
           </p>
         </div>
 
         <button
           onClick={onClick}
-          className="w-50px text-custom-dark font-bold p-2 border-4 border-custom-dark rounded-xl hover:bg-custom-dark hover:text-custom-white transition duration-300 ease-in-out transform hover:scale-105"
+          className="w-1/4 mt-2 text-custom-dark font-bold p-2 border-2 border-custom-dark rounded-xl hover:bg-custom-dark hover:text-custom-white transition duration-300 ease-in-out transform hover:scale-105"
         >
           <i className="fa-solid fa-trash"></i>
         </button>
